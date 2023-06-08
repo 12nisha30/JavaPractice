60f5eae32107968f14e10094aa92df31571b3bc6
Nirosha Nandavarapu <gnisha1230@gmail.com>
2023-06-08 12:25:37 -0500

    this my second code

---
+++ src/main/java/nirosha/WhileLooping/NEvenNum.java
@@ -0,0 +1,22 @@
+package nirosha.WhileLooping;
+
+import java.util.Scanner;
+
+public class NEvenNum {
+    public  void Printeven() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("print the starting number");
+        int count= sc.nextInt();
+        System.out.println("print the n number");
+        int n = sc.nextInt();
+        for(int i = 0; i<n; i++) {
+            if (count % 2 == 0) {
+                System.out.println("print the numbers" + count);
+            }count++;
+        }
+    }
+    public static void main(String[] args){
+        NEvenNum NE = new NEvenNum();
+        NE.Printeven();
+    }
+}
