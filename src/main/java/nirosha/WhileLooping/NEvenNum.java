07073940faafd0c32aeef26f69311d3bedb14288
Nirosha Nandavarapu <gnisha1230@gmail.com>
2023-06-08 12:43:09 -0500

    changed second code

--- src/main/java/nirosha/WhileLooping/NEvenNum.java
+++ src/main/java/nirosha/WhileLooping/NEvenNum.java
@@ -6,13 +6,14 @@ public class NEvenNum {
     public  void Printeven() {
         Scanner sc = new Scanner(System.in);
         System.out.println("print the starting number");
-        int count= sc.nextInt();
+       // int count= sc.nextInt();
         System.out.println("print the n number");
         int n = sc.nextInt();
         for(int i = 0; i<n; i++) {
-            if (count % 2 == 0) {
-                System.out.println("print the numbers" + count);
-            }count++;
+           /* if (i % 2 == 0) {
+                //System.out.println("print the numbers" + count);
+            }count++;*/
+            System.out.println("print the number" + i*2);
         }
     }
     public static void main(String[] args){
